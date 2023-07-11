cf8ae3a3778ec120d3c6cee967394c0910ff0d66
Aditya singh <adityasingh2062000@example.com>
2023-07-11 12:17:39 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request =require('request') 
+const forecast = (latitude,longitude,callback)=>{
+   const url ='http://api.weatherstack.com/current?access_key=56294c4f823d27e13320e521b62359d9&query=' +latitude +',' +longitude + '&units=f'  
+
+   request({url,json:true},(error,{body}) => {
+    if(error){
+         callback('unable to connect with weather service', undefined)
+    }else if(body.error){
+        // console.log("-----",response.body)
+         callback('unable to find location ', undefined)
+    }
+    else{
+           callback(undefined,body.current.weather_descriptions[0] +".It is current " + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degree out.')
+    }
+
+   })
+}
+module.exports=forecast
