c0ca0822584c1ea5b83efd866628dfc48a05f10e
Aditya singh <adityasingh2062000@example.com>
2023-07-11 16:15:14 +0530

    set up app for deployment

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast=require('./utils/forecast')
 
 
 const app = express()
+const port =process.env.PORT || 3000
 
 //define path for express config
 const publicDirectoryPath =path.join(__dirname,'../public')
@@ -113,6 +114,6 @@ app.get('*',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('server is up on 3000.')
+app.listen(port,()=>{
+    console.log('server is up on ' + port)
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request =require('request') 
+
+
 const forecast = (latitude,longitude,callback)=>{
    const url ='http://api.weatherstack.com/current?access_key=56294c4f823d27e13320e521b62359d9&query=' +latitude +',' +longitude + '&units=f'  
 
@@ -10,6 +12,7 @@ const forecast = (latitude,longitude,callback)=>{
          callback('unable to find location ', undefined)
     }
     else{
+     console.log(body.current.weather_descriptions[0])
            callback(undefined,body.current.weather_descriptions[0] +".It is current " + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degree out.')
     }
 
